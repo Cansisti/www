3bb6648270306a0d10d28f3f7d52ad9d05af7b8b
Tomasz <cansisti@outlook.com>
2019-11-13 10:00:18 +0100

    js

---
+++ L1/js/monster.js
@@ -0,0 +1,13 @@
+/*jslint browser: true*/
+
+function give_cookie() {
+    document.getElementById("monster").src = "./img/monster_mmm.png";
+}
+
+function no_cookie() {
+    document.getElementById("monster").src = "./img/cookie1.png";
+}
+
+function eat_cookie() {
+    document.getElementById("monster").src = "./img/yes_sq.png";
+}
